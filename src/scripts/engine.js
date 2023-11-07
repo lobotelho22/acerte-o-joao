05350969219c740423d894a135b5e97924c3e758
lobotelho22 <eduardo.ocara@gmail.com>
2023-11-07 16:53:38 -0300

    feat: implementado estado e funções da engine

--- src/scripts/engine.js
+++ src/scripts/engine.js
@@ -1,1 +1,58 @@
-// alert("Testando a ligação do script...");
+const state = {
+    view: {
+        squares: document.querySelectorAll(".square"),
+        enemy: document.querySelector(".enemy"),
+        timeLeft: document.querySelector("#time-left"),
+        score: document.querySelector("#score"),
+    },
+    values: {
+        gameVelocity: 1000,
+        hitPosition: 0,
+        result: 0,
+        currentTime: 60,
+    },
+    actions: {
+        timerId: setInterval(randomSquare, 1000),
+        countdownTimerId: setInterval(countdown, 1000),
+    }
+};
+
+function countdown () {
+    state.values.currentTime --
+    state.view.timeLeft.textContent = state.values.currentTime;
+
+    if (state.values.currentTime <=0) {
+        clearInterval(state.actions.countdownTimerId);
+        clearInterval(state.actions.timerId);
+        alert("Game Over! Você marcou " + state.values.result + " Pontos");
+    }
+}
+
+function randomSquare() {
+    state.view.squares.forEach((square) => {
+        square.classList.remove("enemy");
+    });
+    let randomNumber = Math.floor(Math.random() * 9);
+    let randomSquare = state.view.squares[randomNumber];
+    randomSquare.classList.add("enemy");
+    state.values.hitPosition = randomSquare.id;
+}
+
+function addListenerHitBox () {
+    state.view.squares.forEach((square) => {
+        square.addEventListener("mousedown", () => {
+            if (square.id === state.values.hitPosition){
+                state.values.result++;
+                state.view.score.textContent = state.values.result * 10;
+                state.values.hitPosition = null;
+            }
+        });
+    });
+}
+
+function init () {
+    console.log(state.view.squares);
+    addListenerHitBox();
+}
+
+init();
